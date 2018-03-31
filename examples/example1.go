51c2c2db0ddfc261eb550994f30badb551897b16
Denis Dyakov <denis.dyakov@gmail.com>
2018-03-31 15:02:04 +0600

    Initial commit

--- examples/example1.go
+++ examples/example1.go
@@ -14,7 +14,7 @@ var lg = logger.NewPackageLogger("main",
 func main() {
 	// Create new connection to i2c-bus on 1 line with address 0x76.
 	// Use i2cdetect utility to find device address over the i2c-bus
-	i2c, err := i2c.NewI2C(0x76, 1)
+	i2c, err := i2c.NewI2C(0x40, 1)
 	if err != nil {
 		lg.Fatal(err)
 	}
@@ -34,6 +34,11 @@ func main() {
 	if err != nil {
 		lg.Fatal(err)
 	}
-
 	lg.Info(b)
+
+	buf1, err := sensor.ReadSerialNumber(i2c)
+	if err != nil {
+		lg.Fatal(err)
+	}
+	lg.Infof("%+v", buf1)
 }
--- si7021.go
+++ si7021.go
@@ -2,20 +2,20 @@ package si7021
 
 import i2c "github.com/d2r2/go-i2c"
 
-const (
-	CMD_REL_HUM_MASTER_MODE       = 0xE5   // Measure Relative Humidity, Hold Master Mode
-	CMD_REL_HUM_NO_MASTER_MODE    = 0xF5   // Measure Relative Humidity, No Hold Master Mode
-	CMD_TEMPRATURE_MASTER_MODE    = 0xE3   // Measure Temperature, Hold Master Mode
-	CMD_TEMPRATURE_NO_MASTER_MODE = 0xF3   // Measure Temperature, No Hold Master Mode
-	CMD_TEMP_FROM_PREVIOUS        = 0xE0   // Read Temperature Value from Previous RH Measurement
-	CMD_RESET                     = 0xFE   // Reset
-	CMD_WRITE_USER_REG_1          = 0xE6   // Write RH/T User Register 1
-	CMD_READ_USER_REG_1           = 0xE7   // Read RH/T User Register 1
-	CMD_WRITE_HEATER_REG          = 0x51   // Write Heater Control Register
-	CMD_READ_HEATER_REG           = 0x51   // Read Heater Control Register
-	CMD_READ_ID_1ST_BYTE          = 0xFA0F // Read Electronic ID 1st Byte
-	CMD_READ_ID_2ND_BYTE          = 0xFCC9 // Read Electronic ID 2nd Byte
-	CMD_READ_FIRMWARE_REV         = 0x84B8 // Read Firmware Revision
+var (
+	CMD_REL_HUM_MASTER_MODE       = []byte{0xE5}       // Measure Relative Humidity, Hold Master Mode
+	CMD_REL_HUM_NO_MASTER_MODE    = []byte{0xF5}       // Measure Relative Humidity, No Hold Master Mode
+	CMD_TEMPRATURE_MASTER_MODE    = []byte{0xE3}       // Measure Temperature, Hold Master Mode
+	CMD_TEMPRATURE_NO_MASTER_MODE = []byte{0xF3}       // Measure Temperature, No Hold Master Mode
+	CMD_TEMP_FROM_PREVIOUS        = []byte{0xE0}       // Read Temperature Value from Previous RH Measurement
+	CMD_RESET                     = []byte{0xFE}       // Reset
+	CMD_WRITE_USER_REG_1          = []byte{0xE6}       // Write RH/T User Register 1
+	CMD_READ_USER_REG_1           = []byte{0xE7}       // Read RH/T User Register 1
+	CMD_WRITE_HEATER_REG          = []byte{0x51}       // Write Heater Control Register
+	CMD_READ_HEATER_REG           = []byte{0x51}       // Read Heater Control Register
+	CMD_READ_ID_1ST_PART          = []byte{0xFA, 0x0F} // Read Electronic ID 1st Byte
+	CMD_READ_ID_2ND_PART          = []byte{0xFC, 0xC9} // Read Electronic ID 2nd Byte
+	CMD_READ_FIRMWARE_REV         = []byte{0x84, 0xB8} // Read Firmware Revision
 )
 
 type Si7021 struct {
@@ -27,13 +27,40 @@ func NewSi7021() *Si7021 {
 }
 
 func (v *Si7021) ReadFirmware(i2c *i2c.I2C) (byte, error) {
-	err := i2c.WriteRegU16LE(0x40, CMD_READ_FIRMWARE_REV)
+	_, err := i2c.WriteBytes(CMD_READ_FIRMWARE_REV)
 	if err != nil {
 		return 0, err
 	}
-	b, err := i2c.ReadRegU8(0x40 | 0x80)
+	buf2 := make([]byte, 1)
+	_, err = i2c.ReadBytes(buf2)
 	if err != nil {
 		return 0, err
 	}
-	return b, nil
+	return buf2[0], nil
+}
+
+func (v *Si7021) ReadSerialNumber(i2c *i2c.I2C) ([]byte, error) {
+	buf2 := make([]byte, 8)
+	_, err := i2c.WriteBytes(CMD_READ_ID_1ST_PART)
+	if err != nil {
+		return nil, err
+	}
+	buf1 := make([]byte, 4)
+	_, err = i2c.ReadBytes(buf1)
+	if err != nil {
+		return nil, err
+	}
+	buf2 = append([]byte{}, buf1[0:]...)
+	_, err = i2c.WriteBytes(CMD_READ_ID_2ND_PART)
+	if err != nil {
+		return nil, err
+	}
+	buf1 = make([]byte, 4)
+	_, err = i2c.ReadBytes(buf1)
+	if err != nil {
+		return nil, err
+	}
+	buf2 = append(buf2, buf1[0:]...)
+	return buf2, nil
+
 }
